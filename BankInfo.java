d929c565c852bff2167512919296b8cbae60ad7a
RAVIND1719 <99536592+RAVIND1719@users.noreply.github.com>
2022-05-03 20:41:57 +0530

    Create BankInfo.java

---
+++ BankInfo.java
@@ -0,0 +1,18 @@
+package org.bank;
+
+public class BankInfo {
+	public void saving() {
+
+		System.out.println("In savings account earn 0.5%-0.75%* interest on daily balances");
+	}
+
+	public void fixed() {
+
+		System.out.println("In Fixed Deposit (FD) account the interest rates ranging between 5% p.a. and 6% p.a. ");
+	}
+
+	public void deposit() {
+
+		System.out.println("The minimum and maximum deposit amount are Rs. 100 and Rs. 5 lakh, respectively");
+	}
+}
