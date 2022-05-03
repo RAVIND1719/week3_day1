ef542d14c47861e547e4903f08463af427b1f334
RAVIND1719 <99536592+RAVIND1719@users.noreply.github.com>
2022-05-03 20:42:49 +0530

    Create College.java

---
+++ College.java
@@ -0,0 +1,18 @@
+package org.college;
+
+public class College {
+	public void collegeName() {
+
+		System.out.println("College Name is Presidency College, Chennai");
+	}
+
+	public void collegeCode() {
+
+		System.out.println("College Code = 1011001");
+	}
+
+	public void collegeRank() {
+
+		System.out.println("College Rank = 7th");
+	}
+}
